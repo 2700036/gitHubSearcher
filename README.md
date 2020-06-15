f014e16a8a5916d7f3a65be95b781cbacc34d3b5
Sergey Biryukov <2700036@mail.ru>
2020-06-15 18:37:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Поиск по GitHub репозиториям
+  
+#  Установка:
+# production
+npm run build
+# develop
+npm run dev
+# gh-pages
+npm run deploy
---
+++ src/scripts/utils/dateConverter.js
@@ -0,0 +1,7 @@
+export default function dateConverter (str){ 
+    const dates = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    const arr = str.split('-').reverse();
+    arr[1] = dates[+arr[1]-1];
+    arr[0].charAt(0) == '0' ? arr[0] = arr[0].charAt(1) : 0;
+    return arr.join(' ')
+  }
